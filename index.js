684b3ad0c0676a12949bb13ad5b81bf513956253
unknown <d3v.tushar@gmail.com>
2022-12-29 19:17:20 +0600

    Patch & Delete function added

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID, ObjectId } = require('bson');
 const app = express();
 const port = process.env.PORT || 4000;
+require('dotenv').config();
 
 
 //Middleware
 app.use(express.json());
 app.use(cors());
 
-//taskPlanner
-//tushar2151
-
 //MongoDB
-const uri = "mongodb+srv://taskPlanner:tushar2151@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 const run = async() =>{
     try{
         const taskCollection = client.db("taskPlanner").collection("alltask");
-
         app.post('/mytask', async(req, res) =>{
             const task = req.body;
             const result = await taskCollection.insertOne(task);
@@ -35,6 +32,27 @@ const run = async() =>{
             const mytask = await cursor.toArray();
             res.send(mytask);
         });
+
+         //API for Editing Task
+    app.put('/mytask/:id', async(req, res) =>{
+        const id = req.params.id;
+        const filter = {_id: ObjectId(id)};
+        const options = { upsert: true };
+        const message = req.body;
+        const updateTask = {$set: {
+            message: message.message
+        }}
+        const result = await taskCollection.updateOne(filter, updateTask, options);
+        res.send(result);
+    });
+
+    //Delete Task
+    app.delete('/mytask/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+    });
 
     }
     finally{
